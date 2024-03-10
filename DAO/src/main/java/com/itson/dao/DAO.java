d65d2375a1ad21a8c81a3fcbf1a8fcc5d035ea95
Abraham Quintana <abraham.quintana233514@potros.itson.edu.mx>
2024-03-10 10:21:24 -0700

    Fachada DAO sin metodos de nota de remision

--- DAO/src/main/java/com/itson/dao/DAO.java
+++ DAO/src/main/java/com/itson/dao/DAO.java
@@ -4,7 +4,11 @@
 
 package com.itson.dao;
 
+import com.itson.dominio.Cliente;
+import com.itson.dominio.Servicio;
 import com.itson.dominio.Usuario;
+import com.itson.interfaces.IClientesDAO;
+import com.itson.interfaces.IServiciosDAO;
 import com.itson.interfaces.IUsuariosDAO;
 
 /**
@@ -13,16 +17,64 @@ import com.itson.interfaces.IUsuariosDAO;
  */
 public class DAO {
 
-    public static void main(String[] args) {
         IUsuariosDAO usuarios = new UsuariosDAO();
-        Usuario usuario=new Usuario();
-        usuario.setNombre("Abraham");
-        usuario.setPass("Gatito");
-        boolean res=usuarios.insertarUsuario(usuario);
-        if(res){
-            System.out.println("insercion exitosa");
-        }else{
-            System.out.println("error en insercion");
+        IClientesDAO clientes = new ClientesDAO();
+        IServiciosDAO servicios = new ServiciosDAO();
+        
+        
+        //Métodos de usuarios
+        public boolean insertarUsuario(Usuario usuario){
+            return this.usuarios.insertarUsuario(usuario);
         }
-    }
+        
+        public boolean eliminarUsuario(Long id){
+            return this.usuarios.eliminarUsuario(id);
+        }
+        
+        public boolean actualizarUsuario(Usuario usuario){
+            return this.usuarios.actualizarUsuario(usuario);
+        }
+        
+        public Usuario buscarUsuario(Long id){
+            return this.usuarios.buscarUsuario(id);
+        }
+        
+        public boolean autenticarUsuario(String nombre, String pass){
+            return this.usuarios.autenticarUsuario(nombre, pass);
+        }
+        
+        //Métodos de clientes
+        public boolean insertarCliente(Cliente cliente){
+            return this.clientes.insertarCliente(cliente);
+        }
+        
+        public boolean eliminarCliente(Long id){
+            return this.clientes.eliminarCliente(id);
+        }
+        
+        public boolean actualizarCliente(Cliente cliente){
+            return this.clientes.actualizarCliente(cliente);
+        }
+        
+        public Cliente buscarCliente(Long id){
+            return this.clientes.buscarCliente(id);
+        }
+        
+        //Métodos de servicios
+        public boolean insertarServicio(Servicio servicio){
+            return this.servicios.insertarServicio(servicio);
+        }
+        
+        public boolean eliminarServicio(Long id){
+            return this.servicios.eliminarServicio(id);
+        }
+        
+        public boolean actualizarServicio(Servicio servicio){
+            return this.servicios.actualizarServicio(servicio);
+        }
+        
+        public Servicio buscarServicio(Long id){
+            return this.servicios.buscarServicio(id);
+        }
+    
 }
