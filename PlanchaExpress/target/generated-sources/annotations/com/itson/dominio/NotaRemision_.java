c84d0a3cde8eaf70c41b4bea17f7866a71af16c0
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-06-02 17:50:45 -0700

    listo jframe pass admin

--- PlanchaExpress/target/generated-sources/annotations/com/itson/dominio/NotaRemision_.java
+++ PlanchaExpress/target/generated-sources/annotations/com/itson/dominio/NotaRemision_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-02T17:45:40", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-02T17:49:27", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(NotaRemision.class)
 public class NotaRemision_ { 
 
